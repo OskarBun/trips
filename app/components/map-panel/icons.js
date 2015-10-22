4efa7235a4438b331a15c1b9d1c47d7d774bbe6d
Peter Bunyan <pete@blueshed.co.uk>
2015-10-22 20:34:21 +0100

    re-icon

--- app/components/map-panel/icons.js
+++ app/components/map-panel/icons.js
@@ -4,9 +4,9 @@ var PRIMARY 	= "#D53F43"; // "#69AEBB";
 var SECONDARY 	= "#297685"; // "#FFDE8D";
 var HILIGHT 	= "#30A349"; // "#F9D068";
 
-var RED_ICON	= "data:image/svg+xml," + encodeURIComponent(flag.split("#fff").join(PRIMARY)); //'//maps.google.com/mapfiles/ms/icons/red-dot.png';
-var BLUE_ICON	= "data:image/svg+xml," + encodeURIComponent(flag.split("#fff").join(SECONDARY)); //'//maps.google.com/mapfiles/ms/icons/blue-dot.png';
-var GREEN_ICON	= "data:image/svg+xml," + encodeURIComponent(flag.split("#fff").join(HILIGHT));//'//maps.google.com/mapfiles/ms/icons/green-dot.png';
+var RED_ICON	= "data:image/svg+xml," + encodeURIComponent(flag.split("#00FF43").join(PRIMARY)); //'//maps.google.com/mapfiles/ms/icons/red-dot.png';
+var BLUE_ICON	= "data:image/svg+xml," + encodeURIComponent(flag.split("#00FF43").join(SECONDARY)); //'//maps.google.com/mapfiles/ms/icons/blue-dot.png';
+var GREEN_ICON	= "data:image/svg+xml," + encodeURIComponent(flag.split("#00FF43").join(HILIGHT));//'//maps.google.com/mapfiles/ms/icons/green-dot.png';
 
 var icons = {
 	GREEN_ICON: GREEN_ICON,
